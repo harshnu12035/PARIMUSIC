bab66827a5d40456ee29d7260b2728dc852a6637
harshnu12035 <harshnu12035@gmail.com>
2025-10-25 14:41:52 +0530

    Update repo.py

--- SACHIN_MUSIC/plugins/bot/repo.py
+++ SACHIN_MUSIC/plugins/bot/repo.py
@@ -24,11 +24,11 @@ async def start(_, msg):
           InlineKeyboardButton("✙ ᴀᴅᴅ ᴍᴇ ✙", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("• ʜᴇʟᴘ •", url="https://t.me/Il_4ST_FIGHTER_lI"),
-          InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="https://t.me/V_VIP_OWNER"),
+          InlineKeyboardButton("• ʜᴇʟᴘ •", url="https://t.me/Theshonaqueen"),
+          InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="https://t.me/Theshonaqueen"),
           ],
                [
-                InlineKeyboardButton("• sᴀɴᴀᴛᴀɴɪ ɴᴇᴛᴡᴏʀᴋ •", url=f"https://t.me/SANATANI_TECH"),
+                InlineKeyboardButton("• sᴀɴᴀᴛᴀɴɪ ɴᴇᴛᴡᴏʀᴋ •", url=f"https://t.me/THE_SHONA_SUPPORT"),
 ],
 [
 InlineKeyboardButton("• ᴏғғɪᴄɪᴀʟ ʙᴏᴛ •", url=f"https://t.me/HIMANSHI_MUSIC_BOT"),
@@ -38,7 +38,7 @@ InlineKeyboardButton("• ᴏғғɪᴄɪᴀʟ ʙᴏᴛ •", url=f"https://t.me/
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://telegra.ph/file/256b87e379dfd405b957d.jpg",
+        photo="https://files.catbox.moe/7nzwqw.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
